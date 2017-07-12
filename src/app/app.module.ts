23816550a957749c25bf5b97e4cabbfb58549ac6
Beven Lee <beven.lee@qantasloyalty.com>
2017-07-12 15:29:36 +1000

    Added dashboard component and links to heroes and dashboard components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,30 @@ import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
 import { AppComponent }  from './app.component';
+import { RouterModule } from '@angular/router';
+import { DashboardComponent } from './dashboard.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent],
+  imports:      [
+    BrowserModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      }
+    ])
+  ],
+  declarations: [ AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent],
   providers:    [ HeroService ],
   bootstrap:    [ AppComponent ]
 })
