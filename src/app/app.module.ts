ca248ac3967eee22b12bd79b6057126aeb3836a8
Beven Lee <beven.lee@qantasloyalty.com>
2017-07-16 19:59:08 +1000

    Changed hard coded heroes to call in memory API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,18 @@ import { HeroService } from './hero.service';
 import { AppComponent }  from './app.component';
 import { DashboardComponent } from './dashboard.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpModule }    from '@angular/http';
+
+// Imports for loading & configuring the in-memory web api
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 @NgModule({
   imports:      [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     AppRoutingModule
   ],
   declarations: [ AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent],
