7564b68551831771157670773eacceac8484536b
Beven Lee <beven.lee@qantasloyalty.com>
2017-07-16 19:39:45 +1000

    Finished the routing section - created individual hero-detail component and seperated the HTML and CSS for all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,28 +5,14 @@ import { HeroesComponent } from './heroes.component';
 import { HeroDetailComponent } from './hero-detail.component';
 import { HeroService } from './hero.service';
 import { AppComponent }  from './app.component';
-import { RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   imports:      [
     BrowserModule,
     FormsModule,
-    RouterModule.forRoot([
-      {
-        path: '',
-        redirectTo: '/dashboard',
-        pathMatch: 'full'
-      },
-      {
-        path: 'heroes',
-        component: HeroesComponent
-      },
-      {
-        path: 'dashboard',
-        component: DashboardComponent
-      }
-    ])
+    AppRoutingModule
   ],
   declarations: [ AppComponent, HeroDetailComponent, HeroesComponent, DashboardComponent],
   providers:    [ HeroService ],
